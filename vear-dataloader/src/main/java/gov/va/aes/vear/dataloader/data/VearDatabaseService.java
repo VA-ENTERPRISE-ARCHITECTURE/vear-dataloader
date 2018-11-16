791b82ae6c53365b49cfbfa1d7d889f30f53dde1
vacoseelar <>
2018-11-16 09:48:17 -0500

    Implemented Batch SQL update methods for both insert and update operations.

--- vear-dataloader/src/main/java/gov/va/aes/vear/dataloader/data/VearDatabaseService.java
+++ vear-dataloader/src/main/java/gov/va/aes/vear/dataloader/data/VearDatabaseService.java
@@ -1,6 +1,8 @@
 package gov.va.aes.vear.dataloader.data;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -9,10 +11,12 @@ import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlTypeValue;
+import org.springframework.jdbc.core.StatementCreatorUtils;
 import org.springframework.stereotype.Component;
 
-import gov.va.aes.vear.dataloader.main.VearDataLoader;
 import gov.va.aes.vear.dataloader.model.DatabaseColumn;
 import gov.va.aes.vear.dataloader.model.TableAndColumnMappingInfo;
 
@@ -40,7 +44,7 @@ public class VearDatabaseService {
 	return false;
     }
 
-    public void processDbRecordInsert(VearDataLoader vearDataLoader, Map<String, Object> excelRecord,
+    public void processDbRecordInsert(Map<String, Object> excelRecord,
 	    TableAndColumnMappingInfo tableAndColumnMappingInfo) {
 	List<Object> insertParams = new ArrayList<>();
 	String sql = "insert into  " + tableAndColumnMappingInfo.getTableName();
@@ -53,9 +57,27 @@ public class VearDatabaseService {
 		colNamesSQL = colNamesSQL + ", " + mapping.getValue().getDbColName();
 	    }
 	    String value = "NULL";
-	    if (excelRecord.get(mapping.getKey()) != null) {
+	    Object columnValue = excelRecord.get(mapping.getKey());
+	    if (columnValue != null) {
 		value = "?";
-		insertParams.add(excelRecord.get(mapping.getKey()));
+
+		if (mapping.getValue().getColumnSize() > 0) {// size truncation
+		    if (columnValue != null && columnValue instanceof String
+			    && ((String) columnValue).getBytes().length > mapping.getValue().getColumnSize()) {
+			int originalSize = ((String) columnValue).getBytes().length;
+			int originalLength = ((String) columnValue).length();
+
+			columnValue = ((String) columnValue).substring(0, mapping.getValue().getColumnSize() - 5);
+			LOG.log(Level.FINE,
+				"Truncating text greater than " + mapping.getValue().getColumnSize()
+					+ " bytes. Column Name:" + mapping.getKey() + " Original: " + originalLength
+					+ "(" + originalSize + ") Truncated: " + ((String) columnValue).length() + "("
+					+ ((String) columnValue).getBytes().length + ")");
+
+		    }
+		}
+
+		insertParams.add(columnValue);
 	    }
 
 	    if (valuesSQLStr == null) {
@@ -75,7 +97,7 @@ public class VearDatabaseService {
 	jdbcTemplate.update(sql, insertParams.toArray(new Object[insertParams.size()]));
     }
 
-    public void processDbRecordUpdate(VearDataLoader vearDataLoader, Map<String, Object> excelRecord,
+    public void processDbRecordUpdate(Map<String, Object> excelRecord,
 	    TableAndColumnMappingInfo tableAndColumnMappingInfo) {
 	List<Object> updateParams = new ArrayList<>();
 	String sql = "update " + tableAndColumnMappingInfo.getTableName() + " set ";
@@ -110,7 +132,7 @@ public class VearDatabaseService {
 		whereSQL = " where " + mapping.getValue().getDbColName() + " =  ?";
 		updateParams.add(excelRecord.get(mapping.getKey()));
 	    } else {
-		whereSQL = " and " + mapping.getValue().getDbColName() + " =  ?";
+		whereSQL = whereSQL + " and " + mapping.getValue().getDbColName() + " =  ?";
 		updateParams.add(excelRecord.get(mapping.getKey()));
 	    }
 
@@ -119,11 +141,157 @@ public class VearDatabaseService {
 
 	LOG.log(Level.FINE, "DB UPDATE SQL :" + sql);
 	LOG.log(Level.FINE, "DB UPDATE PARAMS :" + updateParams.toString());
+
 	// update records
 	jdbcTemplate.update(sql, updateParams.toArray(new Object[updateParams.size()]));
     }
 
-    public Map<String, Object> getDBRecord(VearDataLoader vearDataLoader, Map<String, Object> excelRecord,
+    public String getDbRecordUpdateSQL(TableAndColumnMappingInfo tableAndColumnMappingInfo) {
+
+	String sql = "update " + tableAndColumnMappingInfo.getTableName() + " set ";
+	for (Map.Entry<String, DatabaseColumn> mapping : tableAndColumnMappingInfo.getColumnMappings().entrySet()) {
+	    if (!tableAndColumnMappingInfo.getPkColumnMappings().containsKey(mapping.getKey())) {
+
+		sql = sql.concat(mapping.getValue().getDbColName()).concat(" = ").concat("?").concat(",");
+
+	    }
+	}
+	sql = sql.substring(0, sql.length() - 1);
+	String whereSQL = null;
+	for (Map.Entry<String, DatabaseColumn> mapping : tableAndColumnMappingInfo.getPkColumnMappings().entrySet()) {
+	    if (whereSQL == null) {
+		whereSQL = " where " + mapping.getValue().getDbColName() + " =  ?";
+
+	    } else {
+		whereSQL = whereSQL + " and " + mapping.getValue().getDbColName() + " =  ?";
+
+	    }
+
+	}
+	sql = sql + whereSQL;
+
+	LOG.log(Level.FINE, "DB UPDATE SQL :" + sql);
+
+	return sql;
+
+    }
+
+    public List<Object> getDbRecordUpdateParams(Map<String, Object> excelRecord,
+	    TableAndColumnMappingInfo tableAndColumnMappingInfo) {
+	List<Object> updateParams = new ArrayList<>();
+
+	for (Map.Entry<String, DatabaseColumn> mapping : tableAndColumnMappingInfo.getColumnMappings().entrySet()) {
+	    if (!tableAndColumnMappingInfo.getPkColumnMappings().containsKey(mapping.getKey())) {
+		Object columnValue = excelRecord.get(mapping.getKey());
+		if (mapping.getValue().getColumnSize() > 0) {// size truncation
+		    if (columnValue != null && columnValue instanceof String
+			    && ((String) columnValue).getBytes().length > mapping.getValue().getColumnSize()) {
+			int originalSize = ((String) columnValue).getBytes().length;
+			int originalLength = ((String) columnValue).length();
+
+			columnValue = ((String) columnValue).substring(0, mapping.getValue().getColumnSize() - 5);
+			LOG.log(Level.FINE,
+				"Truncating text greater than " + mapping.getValue().getColumnSize()
+					+ " bytes. Column Name:" + mapping.getKey() + " Original: " + originalLength
+					+ "(" + originalSize + ") Truncated: " + ((String) columnValue).length() + "("
+					+ ((String) columnValue).getBytes().length + ")");
+
+		    }
+		}
+
+		updateParams.add(columnValue);
+
+	    }
+	}
+
+	for (Map.Entry<String, DatabaseColumn> mapping : tableAndColumnMappingInfo.getPkColumnMappings().entrySet()) {
+
+	    updateParams.add(excelRecord.get(mapping.getKey()));
+
+	}
+
+	LOG.log(Level.FINE, "DB UPDATE PARAMS :" + updateParams.toString());
+
+	return updateParams;
+
+    }
+
+    public void insertOrUpdateBatch(String sql, List<List<Object>> paramsList) {
+	jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+
+	    @Override
+	    public void setValues(PreparedStatement ps, int i) throws SQLException {
+		List<Object> params = paramsList.get(i);
+		for (int j = 0; j < params.size(); j++) {
+		    StatementCreatorUtils.setParameterValue(ps, j + 1, SqlTypeValue.TYPE_UNKNOWN, params.get(j));
+		}
+	    }
+
+	    @Override
+	    public int getBatchSize() {
+
+		return paramsList.size();
+	    }
+	});
+    }
+
+    public String getDbRecordInsertSQL(TableAndColumnMappingInfo tableAndColumnMappingInfo) {
+
+	String sql = "insert into  " + tableAndColumnMappingInfo.getTableName();
+	String colNamesSQL = null;
+	String valuesSQLStr = null;
+	for (Map.Entry<String, DatabaseColumn> mapping : tableAndColumnMappingInfo.getColumnMappings().entrySet()) {
+	    if (colNamesSQL == null) {
+		colNamesSQL = " ( " + mapping.getValue().getDbColName();
+	    } else {
+		colNamesSQL = colNamesSQL + ", " + mapping.getValue().getDbColName();
+	    }
+
+	    if (valuesSQLStr == null) {
+		valuesSQLStr = " values ( ?";
+	    } else {
+		valuesSQLStr = valuesSQLStr + " , ?";
+	    }
+
+	}
+	colNamesSQL = colNamesSQL + ")";
+	valuesSQLStr = valuesSQLStr + ")";
+	sql = sql + colNamesSQL + valuesSQLStr;
+	LOG.log(Level.FINE, "DB INSERT SQL :" + sql);
+	return sql;
+    }
+
+    public List<Object> getDbRecordInsertParams(Map<String, Object> excelRecord,
+	    TableAndColumnMappingInfo tableAndColumnMappingInfo) {
+	List<Object> insertParams = new ArrayList<>();
+
+	for (Map.Entry<String, DatabaseColumn> mapping : tableAndColumnMappingInfo.getColumnMappings().entrySet()) {
+
+	    Object columnValue = excelRecord.get(mapping.getKey());
+	    if (mapping.getValue().getColumnSize() > 0) {// size truncation
+		if (columnValue != null && columnValue instanceof String
+			&& ((String) columnValue).getBytes().length > mapping.getValue().getColumnSize()) {
+		    int originalSize = ((String) columnValue).getBytes().length;
+		    int originalLength = ((String) columnValue).length();
+
+		    columnValue = ((String) columnValue).substring(0, mapping.getValue().getColumnSize() - 5);
+		    LOG.log(Level.FINE,
+			    "Truncating text greater than " + mapping.getValue().getColumnSize()
+				    + " bytes. Column Name:" + mapping.getKey() + " Original: " + originalLength + "("
+				    + originalSize + ") Truncated: " + ((String) columnValue).length() + "("
+				    + ((String) columnValue).getBytes().length + ")");
+
+		}
+	    }
+	    insertParams.add(columnValue);
+
+	}
+
+	LOG.log(Level.FINE, "DB INSERT PARAMS :" + insertParams.toString());
+	return insertParams;
+    }
+
+    public Map<String, Object> getDBRecord(Map<String, Object> excelRecord,
 	    TableAndColumnMappingInfo tableAndColumnMappingInfo) {
 	Map<String, Object> dbRecord = null;
 
@@ -190,13 +358,13 @@ public class VearDatabaseService {
 	    tableName = tableAndColumnMappingInfo.getTableName();
 	}
 
-	String sql = "select  " + colNamesSQL + " from " + tableName;
+	String sql = "select  " + colNamesSQL + " from " + tableName;// + " where parent_file = 3200117";
 	LOG.log(Level.FINE, "DB Select SQL for all dbRecords: " + sql);
 
 	try {
 	    dbRecords = jdbcTemplate.queryForList(sql);
 	    LOG.log(Level.FINE, "dbRecords size: " + dbRecords.size());
-	} catch (Exception e) {
+	} catch (Throwable e) {
 	    LOG.log(Level.SEVERE, "Unable to read records from VEAR Database.", e);
 	}
 	return dbRecords;
